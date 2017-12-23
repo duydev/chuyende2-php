eb7f9e0a648e1d3d99d3ab0284347b930472dce4
sinhvien <sinhvien>
2017-12-24 04:18:00 +0700

    LAB8

--- src/thuchanh/LAB8/index.php
+++ src/thuchanh/LAB8/index.php
@@ -10,15 +10,20 @@
 </head>
 <body>
 	<form action="" method="POST" enctype="multipart/form-data">
+		<input type="hidden" name="MAX_FILE_SIZE" value="2000000">
 		<table>
 			<tr>
 				<td>Choose file for upload:</td>
-				<td><input type="file" name="file[]" accept="*.jpg,*.png,*.jpeg" multiple></td>
+				<td><input type="file" name="file[]" accept=".jpg, .png, .jpeg" multiple></td>
 				<td><button type="submit">Submit</button></td>
 			</tr>
 		</table>
 		<br>
 		<?php 
+		$types = [
+			'image/jpeg', 'image/gif', 
+		];
+
 		if( ! empty( $_FILES['file'] ) ) {
 			$files = $_FILES['file'];
 			$num_files = count( $files['name'] );
@@ -31,18 +36,26 @@
 				<p>Err No: <?php echo $files['error'][$i]; ?></p>
 				<p>Size: <?php echo $files['size'][$i]; ?></p>
 				<?php
-				if( ! empty( $files['error'][$i] ) ) {
-					print_r( error_get_last() );
-				} else {
-					if( move_uploaded_file( $files['tmp_name'][$i] , 'upload/'. $files['name'][$i] ) ) {
-						?>
-						<b>Upload thành công.</b>
-						<?php
+				if( in_array( $files['type'][$i], $types ) ) {
+					if( $files['size'][$i] < 2000000 ) {
+						if( move_uploaded_file( $files['tmp_name'][$i] , 'upload/'. $files['name'][$i] ) ) {
+							?>
+							<b>Upload thành công.</b>
+							<?php
+						} else {
+							?>
+							<b>Upload thất bại.</b>
+							<?php
+						}	
 					} else {
-						?>
-						<b>Upload thất bại.</b>
-						<?php
-					}				
+					?>
+						<b>File upload không được lớn hơn 2MB.</b>
+					<?php							
+					}
+				} else {
+					?>
+						<b>Định dạng file không được phép upload.</b>
+					<?php	
 				}
 			}
 		}
