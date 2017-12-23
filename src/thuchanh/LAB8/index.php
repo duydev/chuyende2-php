87a95d620bfefa53cd7689c40ded1175b20c6d95
sinhvien <sinhvien>
2017-12-24 04:09:46 +0700

    Add LAB 8

--- src/thuchanh/LAB8/index.php
+++ src/thuchanh/LAB8/index.php
@@ -5,39 +5,36 @@
 	<title>LAB 8</title>
 	<style>
 		table { border: 1px solid #000; }
+		table td { border: 1px solid #000;  }
 	</style>
 </head>
 <body>
 	<form action="" method="POST" enctype="multipart/form-data">
 		<table>
-			<?php 
-			$num_rows = 7;
-			for ($i = 0; $i < $num_rows; $i++) {
-				
-			}
-			?>
 			<tr>
 				<td>Choose file for upload:</td>
-				<td><input type="file" name="file" id=""></td>
+				<td><input type="file" name="file[]" accept="*.jpg,*.png,*.jpeg" multiple></td>
 				<td><button type="submit">Submit</button></td>
 			</tr>
 		</table>
 		<br>
 		<?php 
 		if( ! empty( $_FILES['file'] ) ) {
-			$file = $_FILES['file'];
+			$files = $_FILES['file'];
+			$num_files = count( $files['name'] );
+			for( $i = 0; $i < $num_files; $i++ ) {
 			?>
 				<h2>File Information</h2>
-				<p>Name: <?php echo $file['name']; ?></p>
-				<p>Type: <?php echo $file['type']; ?></p>
-				<p>Temp Name: <?php echo $file['tmp_name']; ?></p>
-				<p>Err No: <?php echo $file['error']; ?></p>
-				<p>Size: <?php echo $file['size']; ?></p>
+				<p>Name: <?php echo $files['name'][$i]; ?></p>
+				<p>Type: <?php echo $files['type'][$i]; ?></p>
+				<p>Temp Name: <?php echo $files['tmp_name'][$i]; ?></p>
+				<p>Err No: <?php echo $files['error'][$i]; ?></p>
+				<p>Size: <?php echo $files['size'][$i]; ?></p>
 				<?php
-				if( ! empty( $file['error'] ) ) {
+				if( ! empty( $files['error'][$i] ) ) {
 					print_r( error_get_last() );
 				} else {
-					if( move_uploaded_file( $file['tmp_name'] , 'upload/'. $file['name'] ) ) {
+					if( move_uploaded_file( $files['tmp_name'][$i] , 'upload/'. $files['name'][$i] ) ) {
 						?>
 						<b>Upload thành công.</b>
 						<?php
@@ -47,9 +44,8 @@
 						<?php
 					}				
 				}
-
-
 			}
+		}
 		?>
 	</form>
 </body>
